09a094d54fa320692f52547ba4724ed160764e82
Dmitriy Brilov <dmitrijbrilev6@gmail.com>
2022-06-05 20:35:25 +0200

    Init Commit

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
-const mongodb_url = require('../../config/mongodb_url.js')
 
-mongoose.connect(mongodb_url).then(() => {
+mongoose.connect(process.env.mongodb_url).then(() => {
     console.log('Connected to mongoDB')
 }).catch((error) => {
     console.log(error)
